8697b10e312f0de37cc8fbf7e0044762b65ee718
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2026-03-06 14:19:04 +0100

    feat - dynamics components

---
+++ src/components/componenteDinamico.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="texto">
+      <h3>{{ titulo }}</h3>
+      <p>{{ parrafo }}</p>
+    </div>
+
+    <imagen :url="enlace" :texto="alt" :carpeta="carpetaGuardado"></imagen>
+  </div>
+</template>
+<script>
+import imagen from './imagen.vue'
+
+export default {
+  name: 'componenteDinamico',
+  props: {
+    enlace: String,
+    alt: String,
+    carpetaGuardado: String,
+    titulo: String,
+    parrafo: String,
+  },
+  components: {
+    imagen,
+  },
+}
+</script>
+<style scoped>
+div {
+  display: flex;
+  flex-direction: row;
+}
+.texto {
+  flex-direction: column;
+}
+</style>
---
+++ src/views/viewComponentesDinamicos.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>Aqui tenemos un componente dinamico, con dos componentes dentro</h2>
+  <componenteDinamicoPadre></componenteDinamicoPadre>
+</template>
+<script>
+import ComponenteDinamicoPadre from '@/components/componenteDinamicoPadre.vue'
+import componenteDinamicoPadre from '@/components/componenteDinamicoPadre.vue'
+export default {
+  name: 'componenesDinamicos',
+  components: {
+    componenteDinamicoPadre,
+  },
+}
+</script>
+<style></style>
