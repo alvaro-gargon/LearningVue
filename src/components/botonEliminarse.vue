8e17c684db0928b84b25d7d5b3ca356796031f5b
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2026-03-06 08:37:10 +0100

    add - botonAutodestruir

---
+++ src/components/botonEliminarse.vue
@@ -0,0 +1,19 @@
+<template>
+  <button v-if="visible" @click="eliminar">Eliminar botón</button>
+</template>
+
+<script>
+export default {
+  name: 'botonEliminarse',
+  data() {
+    return {
+      visible: true,
+    }
+  },
+  methods: {
+    eliminar() {
+      this.visible = false
+    },
+  },
+}
+</script>
--- src/views/viewPrincipal.vue
+++ src/views/viewPrincipal.vue
@@ -3,6 +3,7 @@
     <h2>
       Esta página esta dedicada a explicar y mostrar el funcionamiento de aspectos básicos de Vue.js
     </h2>
+    <BotonEliminarse></BotonEliminarse>
     <p>
       Cada página tratará sobre un tema, para navegar entre ellas, pulse los botones en la parte
       inferior de la página
@@ -10,9 +11,13 @@
   </div>
 </template>
 <script>
+import BotonEliminarse from '@/components/botonEliminarse.vue'
+
 export default {
   name: 'app',
-  components: {},
+  components: {
+    BotonEliminarse,
+  },
 }
 </script>
 <style></style>
